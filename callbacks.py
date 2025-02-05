20c449716ddf8af677457c4d6fe3fda5153e5d91
Jerod Sun <46911616+Jerodsun@users.noreply.github.com>
2025-02-04 22:54:31 -0500

    add map and tabs

--- callbacks.py
+++ callbacks.py
@@ -3,7 +3,13 @@ from dash import Input, Output
 def register_callbacks(app):
     @app.callback(
         Output('dashboard-content', 'children'),
-        [Input('some-input-id', 'value')]
+        [Input('tabs', 'active_tab')]
     )
-    def update_dashboard(value):
-        return f'Updated dashboard with value: {value}'
+    def update_dashboard(active_tab):
+        if active_tab == 'tab-1':
+            return 'Dashboard content for Tab 1'
+        elif active_tab == 'tab-2':
+            return 'Dashboard content for Tab 2'
+        elif active_tab == 'tab-3':
+            return 'Dashboard content for Tab 3'
+        return 'Select a tab to see dashboard content'
--- layout.py
+++ layout.py
@@ -1,10 +1,31 @@
 import dash_bootstrap_components as dbc
-from dash import html
+from dash import html, dcc
+import plotly.express as px
 
 def create_layout():
+    # Create a map centered around the United States
+    fig = px.scatter_mapbox(
+        lat=[37.0902], lon=[-95.7129], zoom=3,
+        mapbox_style="carto-positron"
+    )
+
     return dbc.Container([
         dbc.Row([
             dbc.Col(html.H1("Readiness Dashboard"), width=12)
+        ]),
+        dbc.Row([
+            dbc.Col(
+                dbc.Tabs([
+                    dbc.Tab(label='Tab 1', tab_id='tab-1'),
+                    dbc.Tab(label='Tab 2', tab_id='tab-2'),
+                    dbc.Tab(label='Tab 3', tab_id='tab-3'),
+                ], id='tabs', active_tab='tab-1'),
+                width=4
+            ),
+            dbc.Col(
+                dcc.Graph(figure=fig),
+                width=8
+            )
         ]),
         dbc.Row([
             dbc.Col(html.Div(id='dashboard-content'), width=12)
