de2b7e8d880a7043e2ddc7356aaf815d352b09bc
Jerod Sun <46911616+Jerodsun@users.noreply.github.com>
2025-02-04 22:47:32 -0500

    setup initial dash app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from dash import Dash
+import dash_bootstrap_components as dbc
+from layout import create_layout
+from callbacks import register_callbacks
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get env variables
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+
+# Initialize the Dash app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# Set the layout of the app
+app.layout = create_layout()
+
+# Register the callbacks
+register_callbacks(app)
+
+# Run the server
+if __name__ == '__main__':
+    app.run_server(debug=DEBUG)
